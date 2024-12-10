e1346cba79653c9ca1535ddc5db2d687cde157b9
angieMor <angiep2008@github.com>
2024-12-10 00:17:48 -0500

    fix: updates key/value validation for the movies objects

--- src/middlewares/validationMiddleware.js
+++ src/middlewares/validationMiddleware.js
@@ -5,7 +5,8 @@ export const validateFavoriteMovieObject = [
     body('Title').isString().not().optional().withMessage('Title is required and must be a string'),
     body('Year').isString().notEmpty().withMessage('Year is required and must be a string'),
     body('imdbID').isString().notEmpty().withMessage('imdbID is required and must be a string'),
-    body('Type').isString().notEmpty().withMessage('Type is required and must be a string'),
+    body('Genre').isString().notEmpty().withMessage('Genre is required and must be a string'),
+    body('Plot').isString().notEmpty().withMessage('Plot is required and must be a string'),
     body('Poster').isURL().withMessage('Poster must be a valid URL'),
     body('Notes').isString().optional(),
 ];
